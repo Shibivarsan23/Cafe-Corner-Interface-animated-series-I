10245be68cb0138a558ed36f4bb167e45d2db455
SHIBIVARSAN J <133083500+Shibivarsan23@users.noreply.github.com>
2024-04-19 13:00:00 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,211 @@
+function runAnimations() {
+  runAnimations_1179();
+}
+runAnimations();
+function runAnimations_1179() {
+  setTimeout(() => {
+    document.querySelector("#id-182").classList.add("animationClass-182");
+    document.querySelector("#id-180").classList.add("animationClass-180");
+    document.querySelector("#id-187").classList.add("animationClass-null187");
+    document
+      .querySelector("#id-187 > div:first-child")
+      .classList.add("bgClass-187");
+    document.querySelector("#id-197").classList.add("nullAnimation-197");
+    document.querySelector("#id-1112").classList.add("nullAnimation-1112");
+    document.querySelector("#id-1124").classList.add("nullAnimation-1124");
+    document.querySelector("#id-1133").classList.add("nullAnimation-1133");
+    document.querySelector("#id-1149").classList.add("nullAnimation-1149");
+    document.querySelector("#id-1158").classList.add("nullAnimation-1158");
+    document.querySelector("#id-1165").classList.add("nullAnimation-1165");
+    document.querySelector("#id-178").classList.add("animationClass-178");
+  }, 800);
+
+  setTimeout(() => {
+    document.querySelector("#id-182").classList.add("animationClass-184");
+    document.querySelector("#id-180").classList.add("animationClass-185");
+    document.querySelector("#id-187").classList.add("animationClass-187");
+    document.querySelector("#id-197").classList.add("animationClass-null197");
+    document
+      .querySelector("#id-197 > div:first-child")
+      .classList.add("bgClass-197");
+    document.querySelector("#id-178").classList.add("nullAnimation-178");
+  }, 1900.0000000000002);
+
+  setTimeout(() => {
+    document.querySelector("#id-182").classList.add("animationClass-195");
+    document.querySelector("#id-180").classList.add("animationClass-196");
+    document.querySelector("#id-187").classList.add("nullAnimation-187");
+    document.querySelector("#id-197").classList.add("animationClass-197");
+    document.querySelector("#id-1112").classList.add("animationClass-null1112");
+    document
+      .querySelector("#id-1112 > div:first-child")
+      .classList.add("bgClass-1112");
+  }, 3000);
+
+  setTimeout(() => {
+    document.querySelector("#id-182").classList.add("animationClass-1106");
+    document.querySelector("#id-180").classList.add("animationClass-1107");
+    document.querySelector("#id-1112").classList.add("animationClass-1112");
+    document.querySelector("#id-1124").classList.add("animationClass-null1124");
+    document
+      .querySelector("#id-1124 > div:first-child")
+      .classList.add("bgClass-1124");
+  }, 4100);
+
+  setTimeout(() => {
+    document.querySelector("#id-182").classList.add("animationClass-1117");
+    document.querySelector("#id-180").classList.add("animationClass-1118");
+    document.querySelector("#id-1124").classList.add("animationClass-1124");
+    document.querySelector("#id-1133").classList.add("animationClass-null1133");
+    document
+      .querySelector("#id-1133 > div:first-child")
+      .classList.add("bgClass-1133");
+  }, 5200);
+
+  setTimeout(() => {
+    document.querySelector("#id-182").classList.add("animationClass-1128");
+    document.querySelector("#id-180").classList.add("animationClass-1129");
+    document.querySelector("#id-1133").classList.add("animationClass-1133");
+    document.querySelector("#id-1149").classList.add("animationClass-null1149");
+    document
+      .querySelector("#id-1149 > div:first-child")
+      .classList.add("bgClass-1149");
+  }, 6300);
+
+  setTimeout(() => {
+    document.querySelector("#id-182").classList.add("animationClass-1139");
+    document.querySelector("#id-180").classList.add("animationClass-1140");
+    document.querySelector("#id-1149").classList.add("animationClass-1149");
+    document.querySelector("#id-1158").classList.add("animationClass-null1158");
+    document
+      .querySelector("#id-1158 > div:first-child")
+      .classList.add("bgClass-1158");
+  }, 7399.999999999999);
+
+  setTimeout(() => {
+    document.querySelector("#id-182").classList.add("animationClass-1150");
+    document.querySelector("#id-180").classList.add("animationClass-1151");
+    document.querySelector("#id-1158").classList.add("animationClass-1158");
+    document.querySelector("#id-1165").classList.add("animationClass-null1165");
+    document
+      .querySelector("#id-1165 > div:first-child")
+      .classList.add("bgClass-1165");
+  }, 8500);
+
+  setTimeout(() => {
+    document.querySelector("#id-182").classList.add("animationClass-1161");
+    document.querySelector("#id-180").classList.add("animationClass-1162");
+    document.querySelector("#id-1165").classList.add("animationClass-1165");
+    document.querySelector("#id-178").classList.add("animationClass-null178");
+    document
+      .querySelector("#id-178 > div:first-child")
+      .classList.add("bgClass-178");
+
+    setTimeout(() => {
+      //loop login
+      document.querySelector("#id-182").classList.remove("animationClass-182");
+      document.querySelector("#id-180").classList.remove("animationClass-180");
+      document
+        .querySelector("#id-187")
+        .classList.remove("animationClass-null187");
+      document
+        .querySelector("#id-187 > div:first-child")
+        .classList.remove("bgClass-187");
+      document.querySelector("#id-197").classList.remove("nullAnimation-197");
+      document.querySelector("#id-1112").classList.remove("nullAnimation-1112");
+      document.querySelector("#id-1124").classList.remove("nullAnimation-1124");
+      document.querySelector("#id-1133").classList.remove("nullAnimation-1133");
+      document.querySelector("#id-1149").classList.remove("nullAnimation-1149");
+      document.querySelector("#id-1158").classList.remove("nullAnimation-1158");
+      document.querySelector("#id-1165").classList.remove("nullAnimation-1165");
+      document.querySelector("#id-178").classList.remove("animationClass-178");
+      document.querySelector("#id-182").classList.remove("animationClass-184");
+      document.querySelector("#id-180").classList.remove("animationClass-185");
+      document.querySelector("#id-187").classList.remove("animationClass-187");
+      document
+        .querySelector("#id-197")
+        .classList.remove("animationClass-null197");
+      document
+        .querySelector("#id-197 > div:first-child")
+        .classList.remove("bgClass-197");
+      document.querySelector("#id-178").classList.remove("nullAnimation-178");
+      document.querySelector("#id-182").classList.remove("animationClass-195");
+      document.querySelector("#id-180").classList.remove("animationClass-196");
+      document.querySelector("#id-187").classList.remove("nullAnimation-187");
+      document.querySelector("#id-197").classList.remove("animationClass-197");
+      document
+        .querySelector("#id-1112")
+        .classList.remove("animationClass-null1112");
+      document
+        .querySelector("#id-1112 > div:first-child")
+        .classList.remove("bgClass-1112");
+      document.querySelector("#id-182").classList.remove("animationClass-1106");
+      document.querySelector("#id-180").classList.remove("animationClass-1107");
+      document
+        .querySelector("#id-1112")
+        .classList.remove("animationClass-1112");
+      document
+        .querySelector("#id-1124")
+        .classList.remove("animationClass-null1124");
+      document
+        .querySelector("#id-1124 > div:first-child")
+        .classList.remove("bgClass-1124");
+      document.querySelector("#id-182").classList.remove("animationClass-1117");
+      document.querySelector("#id-180").classList.remove("animationClass-1118");
+      document
+        .querySelector("#id-1124")
+        .classList.remove("animationClass-1124");
+      document
+        .querySelector("#id-1133")
+        .classList.remove("animationClass-null1133");
+      document
+        .querySelector("#id-1133 > div:first-child")
+        .classList.remove("bgClass-1133");
+      document.querySelector("#id-182").classList.remove("animationClass-1128");
+      document.querySelector("#id-180").classList.remove("animationClass-1129");
+      document
+        .querySelector("#id-1133")
+        .classList.remove("animationClass-1133");
+      document
+        .querySelector("#id-1149")
+        .classList.remove("animationClass-null1149");
+      document
+        .querySelector("#id-1149 > div:first-child")
+        .classList.remove("bgClass-1149");
+      document.querySelector("#id-182").classList.remove("animationClass-1139");
+      document.querySelector("#id-180").classList.remove("animationClass-1140");
+      document
+        .querySelector("#id-1149")
+        .classList.remove("animationClass-1149");
+      document
+        .querySelector("#id-1158")
+        .classList.remove("animationClass-null1158");
+      document
+        .querySelector("#id-1158 > div:first-child")
+        .classList.remove("bgClass-1158");
+      document.querySelector("#id-182").classList.remove("animationClass-1150");
+      document.querySelector("#id-180").classList.remove("animationClass-1151");
+      document
+        .querySelector("#id-1158")
+        .classList.remove("animationClass-1158");
+      document
+        .querySelector("#id-1165")
+        .classList.remove("animationClass-null1165");
+      document
+        .querySelector("#id-1165 > div:first-child")
+        .classList.remove("bgClass-1165");
+      document.querySelector("#id-182").classList.remove("animationClass-1161");
+      document.querySelector("#id-180").classList.remove("animationClass-1162");
+      document
+        .querySelector("#id-1165")
+        .classList.remove("animationClass-1165");
+      document
+        .querySelector("#id-178")
+        .classList.remove("animationClass-null178");
+      document
+        .querySelector("#id-178 > div:first-child")
+        .classList.remove("bgClass-178");
+      runAnimations_1179();
+    }, 300);
+  }, 9600.000000000002);
+}
